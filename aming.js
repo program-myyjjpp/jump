1a8f3ad718986805978df71f41ef8df45417e250
MYY <liran61468384@gmail.com>
2025-06-13 21:06:36 +0800

    Create aming.js

---
+++ aming.js
@@ -0,0 +1,2 @@
+document.write('<script charset="UTF-8" id="LA_COLLECT" src="https://sdk.51.la/js-sdk-pro.min.js"></script>');
+document.write('<script>LA.init({id:"Kn7wIzRfPyfLVhKi",ck:"Kn7wIzRfPyfLVhKi"});</script>');
