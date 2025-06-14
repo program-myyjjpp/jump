86f1f2f48204d924ce084d8c985736bda0fffef5
MYY <liran61468384@gmail.com>
2025-06-14 12:22:52 +0800

    Update aming.js

--- aming.js
+++ aming.js
@@ -1,23 +1,9 @@
-document.write('<script charset="UTF-8" id="LA_COLLECT" src="https://sdk.51.la/js-sdk-pro.min.js"></script>');
-document.write('<script>LA.init({id:"Kn7wIzRfPyfLVhKi",ck:"Kn7wIzRfPyfLVhKi"});</script>');
-window.addEventListener('DOMContentLoaded', function() {
-  const iframe = document.createElement('iframe');
-  iframe.src = 'https://www.myyjjpp.com';
-  iframe.style.position = 'absolute';
-  iframe.style.top = '0';
-  iframe.style.left = '0';
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-  iframe.style.border = 'none';
-  iframe.setAttribute('allowfullscreen', '');
-  iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms');
-
-  document.documentElement.style.margin = '0';
-  document.documentElement.style.padding = '0';
-  document.documentElement.style.height = '100%';
-  document.body.style.margin = '0';
-  document.body.style.padding = '0';
-  document.body.style.height = '100%';
-
-  document.body.appendChild(iframe);
-});
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://sdk.51.la/js-sdk-pro.min.js";
+  script.async = true;
+  script.onload = function () {
+    LA.init({ id: "Kn7wIzRfPyfLVhKi", ck: "Kn7wIzRfPyfLVhKi" });
+  };
+  document.head.appendChild(script);
+})();
