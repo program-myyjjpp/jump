cdfbc0318f764b8a7cc31885a2c4c703ce050b18
MYY <liran61468384@gmail.com>
2025-06-14 12:54:57 +0800

    Update aming-403.js

--- aming-403.js
+++ aming-403.js
@@ -1,1 +1,10 @@
 
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://sdk.51.la/js-sdk-pro.min.js";
+  script.async = true;
+  script.onload = function () {
+    LA.init({ id: "Kn7wIzRfPyfLVhKi", ck: "Kn7wIzRfPyfLVhKi" });
+  };
+  document.head.appendChild(script);
+})();
